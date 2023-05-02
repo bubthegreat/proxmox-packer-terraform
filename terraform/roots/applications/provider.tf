9f2f4f8c9d4a361b8e91126e7ade47e6375c4777
Micheal Taylor <bubthegreat@gmail.com>
2023-05-02 01:28:52 -0600

    Removing packer templates for now

--- terraform/roots/applications/provider.tf
+++ terraform/roots/applications/provider.tf
@@ -1,10 +1,6 @@
 
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.65.0"
-    }
     helm = {
       source  = "hashicorp/helm"
       version = "2.3.0"
@@ -14,12 +10,6 @@ terraform {
       version = "1.14.0"
     }
   }
-}
-
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
 
 provider "helm" {
